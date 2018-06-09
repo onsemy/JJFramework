fc5e4d38b1e631170229880a0dd0121a8b4247f3
oNsemy <onsemy@gmail.com>
2018-06-09 12:16:58 +0900

    ExMonoBehaviour 추가
    * Awake시 LoadComponents를 하도록 추가

---
+++ Runtime/Extension/ExMonoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+
+namespace JJFramework.Runtime.Extension
+{
+    [DisallowMultipleComponent]
+    public class ExMonoBehaviour : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            this.LoadComponents();
+        }
+    }
+}
