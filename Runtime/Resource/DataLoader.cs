632817e7d1f4716996d27bb66aee21de7ae5c70c
Jeongjin Oh <onsemy@gmail.com>
2019-01-17 00:18:50 +0900

    Fix condition
    * Remove Exist Condition check

--- Runtime/Resource/DataLoader.cs
+++ Runtime/Resource/DataLoader.cs
@@ -19,38 +19,35 @@ namespace JJFramework.Runtime.Resource
         {
             string fileName = $"{typeof(T).Name}.bson";
             string destinationPath = System.IO.Path.Combine(Application.persistentDataPath, fileName);
-            if (File.Exists(destinationPath) == false)
-            {
-                string sourcePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+            string sourcePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
 
-                //if DB does not exist in persistent data folder (folder "Documents" on iOS) or source DB is newer then copy it
-                if (!System.IO.File.Exists(destinationPath) || (System.IO.File.GetLastWriteTimeUtc(sourcePath) > System.IO.File.GetLastWriteTimeUtc(destinationPath)))
-                {
-                    if (sourcePath.Contains("://"))
-                    {// Android  
-                        var www = new WWW(sourcePath);
-                        while (!www.isDone) {; }                // Wait for download to complete - not pretty at all but easy hack for now 
-                        if (string.IsNullOrEmpty(www.error))
-                        {
-                            System.IO.File.WriteAllBytes(destinationPath, www.bytes);
-                        }
-                        else
-                        {
-                            Debug.Log("ERROR: the file DB named " + fileName + " doesn't exist in the StreamingAssets Folder, please copy it there.");
-                        }
+            //if DB does not exist in persistent data folder (folder "Documents" on iOS) or source DB is newer then copy it
+            if (!System.IO.File.Exists(destinationPath) || (System.IO.File.GetLastWriteTimeUtc(sourcePath) > System.IO.File.GetLastWriteTimeUtc(destinationPath)))
+            {
+                if (sourcePath.Contains("://"))
+                {// Android  
+                    var www = new WWW(sourcePath);
+                    while (!www.isDone) {; }                // Wait for download to complete - not pretty at all but easy hack for now 
+                    if (string.IsNullOrEmpty(www.error))
+                    {
+                        System.IO.File.WriteAllBytes(destinationPath, www.bytes);
                     }
                     else
-                    {                // Mac, Windows, Iphone                
-                                     //validate the existens of the DB in the original folder (folder "streamingAssets")
-                        if (System.IO.File.Exists(sourcePath))
-                        {
-                            //copy file - alle systems except Android
-                            System.IO.File.Copy(sourcePath, destinationPath, true);
-                        }
-                        else
-                        {
-                            Debug.Log("ERROR: the file DB named " + fileName + " doesn't exist in the StreamingAssets Folder, please copy it there.");
-                        }
+                    {
+                        Debug.Log("ERROR: the file DB named " + fileName + " doesn't exist in the StreamingAssets Folder, please copy it there.");
+                    }
+                }
+                else
+                {                // Mac, Windows, Iphone                
+                                 //validate the existens of the DB in the original folder (folder "streamingAssets")
+                    if (System.IO.File.Exists(sourcePath))
+                    {
+                        //copy file - alle systems except Android
+                        System.IO.File.Copy(sourcePath, destinationPath, true);
+                    }
+                    else
+                    {
+                        Debug.Log("ERROR: the file DB named " + fileName + " doesn't exist in the StreamingAssets Folder, please copy it there.");
                     }
                 }
             }
