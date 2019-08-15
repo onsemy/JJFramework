57427421007c42c143de8b61939a4423c5f15539
Jeongjin Oh <onsemy@gmail.com>
2019-08-15 17:25:34 +0900

    MonoBehaviour 기반의 Singleton 추가

---
+++ Runtime/MonoSingleton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JJFramework.Runtime
+{
+	public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static object _lock = new object();
+		private static T _instance;
+
+		public static T Instance
+		{
+			get
+			{
+#if !UNITY_EDITOR
+				if (_applicationQuit)
+				{
+					return null;
+				}
+#endif
+				lock (_lock)
+				{
+					if (ReferenceEquals(_instance, null))
+					{
+						_instance = (T) FindObjectOfType(typeof(T));
+
+						if (ReferenceEquals(_instance, null))
+						{
+							GameObject singletonObject = new GameObject($"(MonoSingleton) {typeof(T).Name}");
+							_instance = singletonObject.AddComponent<T>();
+							
+							DontDestroyOnLoad(singletonObject);
+							
+							Debug.Log($"[MonoSingleton] Created instance of {typeof(T).Name}");
+						}
+					}
+
+					return _instance;
+				}
+			}
+		}
+
+		private static bool _applicationQuit = false;
+
+		private void OnApplicationQuit()
+		{
+			_applicationQuit = true;
+		}
+
+		private void OnDestroy()
+		{
+			_applicationQuit = true;
+		}
+	}
+}
