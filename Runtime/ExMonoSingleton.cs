01487976e34792b797e59fb198fca3e105732ea3
Jeongjin Oh <onsemy@gmail.com>
2019-10-23 19:40:53 +0900

    ExMonoBehaviour용 Singleton 추가

---
+++ Runtime/ExMonoSingleton.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JJFramework.Runtime.Extension;
+using UniRx;
+
+namespace JJFramework.Runtime
+{
+    public class ExMonoSingleton<T> : ExMonoBehaviour where T : ExMonoBehaviour
+    {
+        private static object _lock = new object();
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+#if !UNITY_EDITOR
+				if (_applicationQuit)
+				{
+					return null;
+				}
+#endif
+                lock (_lock)
+                {
+                    if (ReferenceEquals(_instance, null))
+                    {
+                        _instance = (T) FindObjectOfType(typeof(T));
+
+                        if (ReferenceEquals(_instance, null))
+                        {
+                            GameObject singletonObject = new GameObject($"(ExMonoSingleton) {typeof(T).Name}");
+                            _instance = singletonObject.AddComponent<T>();
+							
+                            DontDestroyOnLoad(singletonObject);
+							
+                            Debug.Log($"[MonoSingleton] Created instance of {typeof(T).Name}");
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+#if !UNITY_EDITOR
+		private static bool _applicationQuit = false;
+
+		private void OnApplicationQuit()
+		{
+			_applicationQuit = true;
+		}
+
+		private void OnDestroy()
+		{
+			_applicationQuit = true;
+		}
+#endif
+    }
+}
