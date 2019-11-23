ede4013c527073539718eae58e437a162234e18c
Jeongjin Oh <onsemy@gmail.com>
2019-11-24 01:00:19 +0900

    JJUtils 추가
    * 여러 프로젝트에서 범용으로 쓸만한 함수를 사용할 수 있는 클래스 추가
    * 첫번째 함수로 16진수의 Color 데이터를 Unity의 Color32로 변환하는 기능 추가

---
+++ Runtime/Util/JJUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace JJFramework.Runtime.Util
+{
+    public class JJUtils
+    {
+        public static Color32 HexToColor(string hex)
+        {
+            hex = hex.Replace("0x", "");
+            hex = hex.Replace("#", "");
+
+            if (hex.Length < 6 ||
+                hex.Length > 8)
+            {
+                Debug.LogError($"[JJUtils|HexToColor] Parameter is invalid! - {hex}");
+                return new Color32();
+            }
+            
+            var r = byte.Parse(hex.Substring(0, 2), NumberStyles.HexNumber);
+            var g = byte.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
+            var b = byte.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
+            byte a = 255;
+            if (hex.Length == 8)
+            {
+                a = byte.Parse(hex.Substring(6, 2), NumberStyles.HexNumber);
+            }
+            
+            return new Color32(r, g, b, a);
+        }
+    }
+}
